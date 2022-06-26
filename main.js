df287e998939ddfcdce2fa8690326d746f5c1ede
Vickyaadhavv <102547223+Vickyaadhavv@users.noreply.github.com>
2022-06-26 12:16:24 +0530

    e

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then(Response=>Response.json())
-  .then(data=>{console.log(data)
-    const res=data;
-    
+  .then(data=>{const res=data;
   document.getElementById("us").innerHTML=res.userId;
   document.getElementById("id").innerHTML=res.id;
   document.getElementById("tit").innerHTML=res.title;
